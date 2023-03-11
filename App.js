3bcc601f59b22893c01d612f5969dbdd283fb757
sandeep9999 <sandeepdokala@gmail.com>
2023-03-11 10:29:39 +0530

    HomePage UI ONLY ( NOT FIXED STATUS BAR )

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.header}>
+          <Text style={styles.headerText}>MediaConnect</Text>
+        </View>
+        <View style={styles.imgLogo}>
+          <Text style={styles.textInc}>There will be an image here</Text>
+        </View>
+        <View style={styles.btns}>
+          <Pressable style={styles.btnParent}><Text style={styles.btnChild}>Host a party</Text></Pressable>
+          <Pressable style={styles.btnParent}><Text style={styles.btnChild}>Join a party</Text></Pressable>
+        </View>
+        {/* <StatusBar style='light'></StatusBar> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#9C13BC",
+    width: "100%",
+    // height: 78,
+    padding: 20,
+    flex: 1
+  },
+  headerText: {
+    fontSize: 24,
+    color: "white",
+    fontWeight: "600",
+  },
   container: {
-    flex: 1,
+    marginTop: 30,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    flex: 1,
   },
+  imgLogo: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 2,
+    marginTop: 20,
+    marginBottom: 20,
+    flex: 10
+  },
+  textInc: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  btns: {
+    marginVertical: 30,
+    flex: 5
+  },
+  btnParent: {
+    backgroundColor: "#9C13BC",
+    paddingVertical: 20,
+    paddingHorizontal: 60,
+    marginVertical: 16,
+    borderRadius: 50
+  },
+  btnChild: {
+    color: "#fff"
+  }
 });
